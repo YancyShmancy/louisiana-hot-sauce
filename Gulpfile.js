a6cfd86067a3e17b2289c43e96c1b6dc18def267
Zoey Yancy <yancyshmancy@gmail.com>
2016-03-04 03:53:35 -0500

    Finished html skeleton

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,21 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
+var wrench = require('wrench');
+var options = {
+  src: './',
+  dist: 'build',
+  tmp: '.tmp',
+  imagesDir: 'assets/img',
+  fontsDir: 'assets/fonts',
+  jsDir: 'assets/js'
+};
 
-var browserSync = require('browser-sync').create();
-
-// Static
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: "./"
-    });
-    gulp.watch("assets/sass/**/*.scss", ['sass']);
-	gulp.watch("*.html").on('change', browserSync.reload);
+// gulp wrench pulls in all files from gulp dir and requires them
+wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file)(options);
 });
 
-gulp.task('sass', function() {
-    return gulp.src("assets/sass/**/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest("assets/css/"))
-        .pipe(browserSync.stream());
+gulp.task('default', ['clean'], function() {
+  gulp.start('serve');
 });
-
-gulp.task('default', ['serve']);
